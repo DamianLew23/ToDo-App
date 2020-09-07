df6f3d29548b9c8ea1ec30a3f2465ddaa3d5d188
Damian Lew <damian@MacBook-Pro.local>
2020-09-07 16:41:36 +0200

    basic structure ready

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import AddTask from "./AddTask";
+import TaskList from "./TaskList";
 
 function App() {
-  return <div className="App">TODO APP</div>;
+  return (
+    <div className="App">
+      TODO APP
+      <AddTask />
+      <TaskList />
+    </div>
+  );
 }
 
 export default App;
